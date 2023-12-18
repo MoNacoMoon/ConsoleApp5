a086fbddbed98395e48734fe0e7b15f34d69fbb9
EvoL_ <EvoL_@MOON>
2023-12-18 15:32:23 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите целое значение");
+            //int x = int.Parse(Console.ReadLine());
+            //double y = 0;
+            //if (x >= 0)
+            //{
+            //    /*y = Math.Sin(x) * Math.Sin(x);*/
+            //    y = Math.Pow(Math.Sin(x), 2);
+            //}
+            //else
+            //{
+            //    /*y = 1 - 2 * Math.Sin(x * x);*/
+            //    y = 1 - 2 * Math.Sin(Math.Pow(x, 2));
+            //}
+            //Console.WriteLine($"y={y:f2}");
+
+            //Random rnd = new Random();
+            //int number = rnd.Next(10, 99);
+            //Console.WriteLine($"number={ number}");
+            //string stroka = (number % 2 == 0) ? "четное число" : "Нечетное число ";
+            //Console.WriteLine($"{stroka}");
+
+            //Console.WriteLine("Введите 1 число");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите 2 число");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите 3 число");
+            //int c = int.Parse(Console.ReadLine());
+            //if (a % 2 == 0 && b % 2 == 0 && c % 2 == 0)
+            //{
+            //    a += 1;
+            //    b += 1;
+            //    c += 1;
+            //}
+            //else if (a % 2 == 0 || b % 2 == 0 || c % 2 == 0)
+            //{
+            //    a -= 1;
+            //    b -= 1;
+            //    c -= 1;
+            //}
+            //else
+            //{
+            //    a *= 2;
+            //    b *= 2;
+            //    c *= 2;
+
+            //}
+            //Console.WriteLine($"Результат: {a}, {b}, {c}");
+
+            //Console.WriteLine("Введите номер месяца (от 1 до 12):");
+            //int monthNumber = int.Parse(Console.ReadLine());
+            //string season;
+            //switch (monthNumber)
+            //{
+            //    case 12:
+            //    case 1:
+            //    case 2:
+            //        season = "зима";
+            //        break;
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        season = "весна";
+            //        break;
+            //    case 6:
+            //    case 7:
+            //    case 8:
+            //        season = "лето";
+            //        break;
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //        season = "осень";
+            //        break;
+            //    default:
+            //        season = "неизвестная пора года";
+            //        break;
+            //}
+
+            //Console.WriteLine($"Месяц номер {monthNumber} соответствует поре года: {season}");
+            //switch (monthNumber)
+            //{
+            //    case 1:
+            //    case 7:
+            //    case 8:
+            //        Console.WriteLine("Сейчас идут каникулы.");
+            //        break;
+            //    case 2:
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //    case 6:
+            //        Console.WriteLine("Сейчас идет 2 семестр.");
+            //        break;
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //    case 12:
+            //        Console.WriteLine("Сейчас идет 1 семестр.");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Некорректный месяц для определения сессии/каникул.");
+            //        break;
+            
+            //}
+            //        Console.ReadKey();
+        } 
+        }
+    }
+
+
+
+
+
